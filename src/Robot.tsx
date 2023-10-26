fa63173ac743034267440429e4b9ffaf9c4a034c
Nuria <nuria.rs96@gmail.com>
2023-10-26 14:06:44 +0100

    core: create Robot template

---
+++ src/Robot.tsx
@@ -0,0 +1,47 @@
+import classNames from 'classnames';
+import {Coordinate, OrientationEnumType, Robot as RobotType} from  './entityTypes';
+
+type RobotProps = {
+    robot: RobotType;
+}
+
+const RobotInfo = (props: {coord: Coordinate, orientation: OrientationEnumType, path?: string, isDisplayLabels?: boolean, isLost?: boolean, className?: string}) => {
+    const {coord, orientation, path, isDisplayLabels, isLost, className} = props;
+    return (
+        <div className={classNames("flex gap-4 flex-wrap", className)}>
+            <div className="flex gap-2">{isDisplayLabels && <div className="font-bold">X:</div>}{coord.x}</div>
+            <div className="flex gap-2">{isDisplayLabels && <div className="font-bold">Y:</div>}{coord.y}</div>
+            <div className="flex gap-2">{isDisplayLabels && <div className="font-bold">O:</div>}{orientation}</div>
+            {path && <div className="flex gap-2">
+                {isDisplayLabels && <div className="font-bold">P: </div>}{path}</div>}
+            {isLost && <div>LOST</div>}
+        </div>
+    )
+}
+
+function Robot(props: RobotProps) {
+    const {robot} = props;
+    const {id, coord, orientation, path, finalDestination} = robot;
+
+    return (
+        <div className="flex gap-4 flex-wrap flex-col lg:flex-row">
+            <div className="flex flex-[2] gap-4 p-2 rounded bg-gray-100">
+                <h3 className="font-bold border-gray-400 border-r-2 pr-2">{`R${id + 1}`}</h3>
+                <RobotInfo coord={coord} orientation={orientation} path={path} className="flex-1" isDisplayLabels/>
+            </div>
+            {(finalDestination?.coord.x && finalDestination.coord.y && finalDestination.orientation) && (
+                <RobotInfo 
+                    coord={finalDestination.coord} 
+                    orientation={finalDestination.orientation} 
+                    isLost={finalDestination.isLost} 
+                    className={classNames("flex-1 p-2 rounded", {
+                        "bg-green-200": robot.finalDestination && !robot.finalDestination.isLost,
+                        "bg-red-100": robot.finalDestination?.isLost,
+                    })} 
+                />
+            )}
+        </div>
+    );
+}
+
+export default Robot;
