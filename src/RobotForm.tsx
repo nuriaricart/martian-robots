c92199823f58dce7bde4722c92f00e388971e5de
Nuria <nuria.rs96@gmail.com>
2023-10-26 14:09:09 +0100

    core: update robot form

--- src/RobotForm.tsx
+++ src/RobotForm.tsx
@@ -1,20 +1,54 @@
-import {  useState } from "react";
+import { useCallback, useState } from "react";
 import InputWrapper from "./InputWrapper";
-import { OrientationEnumType, Robot } from "./entityTypes";
+import { Coordinate, OrientationEnum, OrientationEnumType, Robot } from "./entityTypes";
+import Button from "./Button";
+import { isValidRobot } from "./helpers";
 
-function RobotForm() {
+type RobotFormProps = {
+    saveRobot: (robot: Robot) => void;
+    gridSize: Coordinate;
+}
+
+const OrientationInput = (props: {setProp: (input: string, key: string) => void, orientation: OrientationEnumType}) => {
+    const {setProp, orientation} = props;
+    return (
+        <div className="flex">
+            <div className="flex-1">Orientation</div>
+            <div className="flex justify-between flex-[2]">
+                {Object.keys(OrientationEnum).map((_orientation, index) => OrientationEnum[index] && 
+                    <InputWrapper 
+                        key={index} 
+                        name="orientation" 
+                        type="radio" 
+                        labelText={OrientationEnum[index]} 
+                        value={OrientationEnum[index]} 
+                        keyValue="orientation" 
+                        setValue={setProp} 
+                        checked={OrientationEnum[index] == orientation} 
+                    /> 
+                )}
+            </div>
+        </div>
+    );
+}
+
+function RobotForm(props: RobotFormProps) {
+    const { saveRobot, gridSize } = props;
     const [robot, setRobot] = useState<Robot>({
-        cord: {
+        id: 0,
+        coord: {
             x: '',
-            y: '',
-        }
+            y: ''
+        },
+        orientation: "N",
+        path: ''
     });
 
-    const setCord = (input: string, key: string) => {
+    const setCoord = (input: string, key: string) => {
         const updatedRobot = {
             ...robot,
-            cord: {
-                ...robot.cord,
+            coord: {
+                ...robot.coord,
                 [key]: input
             }
         };
@@ -29,15 +63,20 @@ function RobotForm() {
         setRobot(updatedRobot);
     };
 
+    const isValidRobotCallback = useCallback(() => {
+        return isValidRobot(gridSize, robot);
+    }, [robot]);
+
     return (
-        <div>
-            <InputWrapper type="number" labelText={"Start X"} value={robot.cord.x} keyValue="x" setValue={setCord}  />
-            <InputWrapper type="number" labelText={"Start Y"} value={robot.cord.y} keyValue="y" setValue={setCord}  />
-            <InputWrapper type="text" labelText={"Start Orientation"} value={robot.orientation ?? ''} keyValue="orientation" setValue={setProp}  />
-            <InputWrapper type="text" labelText={"Robot Path"} value={robot.path ?? ''} keyValue="path" setValue={setProp}  />
+        <div className="flex flex-col gap-4 w-full md:w-2/5 p-8 rounded-md shadow-md">
+            <h2 className="text-lg font-bold">NEW ROBOT</h2>
+            <InputWrapper type="number" labelText={"Start X"} value={robot.coord.x} keyValue="x" setValue={setCoord} classNames={{labelWrapperClassName: "items-center", labelClassName:"flex-1", inputClassName:"flex-[2]" }} />
+            <InputWrapper type="number" labelText={"Start Y"} value={robot.coord.y} keyValue="y" setValue={setCoord} classNames={{labelWrapperClassName: "items-center", labelClassName:"flex-1", inputClassName:"flex-[2]" }} />
+            <OrientationInput setProp={setProp} orientation={robot.orientation} />
+            <InputWrapper type="text" labelText={"Robot Path"} value={robot.path} keyValue="path" setValue={setProp} classNames={{labelWrapperClassName: "items-center", labelClassName:"flex-1", inputClassName:"flex-[2]" }} />
+            <Button label="Save your Robot" className="mt-auto" onClick={() => {saveRobot(robot)}} disabled={!isValidRobotCallback()} />
         </div>
     );
 }
-
 
 export default RobotForm;
